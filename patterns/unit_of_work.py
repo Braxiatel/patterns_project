4712cea61eb584ab5b5cf601d19572adfd3d5194
Braxiatel <kheliz404@gmail.com>
2021-05-31 23:22:56 +0300

    make sure that objects are deleted from the list after insert/remove/update

--- patterns/unit_of_work.py
+++ patterns/unit_of_work.py
@@ -18,36 +18,47 @@ class UnitOfWork:
 
     # append 'new' object to new_objects list
     def register_new(self, obj):
-        self.new_objects.clear()
         self.new_objects.append(obj)
 
     # append updated objects to dirty_objects list
     def register_dirty(self, obj):
-        self.dirty_objects.clear()
         self.dirty_objects.append(obj)
 
     # append objects to be removed to removed_objects list
     def register_removed(self, obj):
-        self.removed_objects.clear()
         self.removed_objects.append(obj)
 
     def insert_new(self):
         # if new_objects list is not cleared here, it is inserted two times
         logger.log(f'Inserting new objects: {self.new_objects}')
-        for obj in self.new_objects:
-            logger.log(f'Getting {self.MapperRegistry} map registry')
-            self.MapperRegistry.get_mapper(obj).insert(obj)
-        self.new_objects.clear()
+        try:
+            for obj in self.new_objects:
+                logger.log(f'Getting {self.MapperRegistry} map registry')
+                self.MapperRegistry.get_mapper(obj).insert(obj)
+        except Exception as e:
+            raise Exception(e)
+        finally:
+            self.new_objects.clear()
 
     def update_dirty(self):
-        logger.log(f'Updating objects: {self.dirty_objects}')
-        for obj in self.dirty_objects:
-            self.MapperRegistry.get_mapper(obj).update(obj)
+        try:
+            logger.log(f'Updating objects: {self.dirty_objects}')
+            for obj in self.dirty_objects:
+                self.MapperRegistry.get_mapper(obj).update(obj)
+        except Exception as e:
+            raise Exception(e)
+        finally:
+            self.dirty_objects.clear()
 
     def delete_removed(self):
-        logger.log(f'Removing objects: {self.removed_objects}')
-        for obj in self.removed_objects:
-            self.MapperRegistry.get_mapper(obj).delete(obj)
+        try:
+            logger.log(f'Removing objects: {self.removed_objects}')
+            for obj in self.removed_objects:
+                self.MapperRegistry.get_mapper(obj).delete(obj)
+        except Exception as e:
+            raise Exception(e)
+        finally:
+            self.removed_objects.clear()
 
     # committing all operations in db in one go
     def commit(self):
@@ -58,7 +69,6 @@ class UnitOfWork:
         self.new_objects.clear()
         self.dirty_objects.clear()
         self.removed_objects.clear()
-
 
     @staticmethod
     def new_thread():
